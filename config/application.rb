b4403be83327349945200e5a9fff539c2d74817a
Marcelo Boeira <me@marceloboeira.com>
2015-05-17 14:02:52 -0300

    [#3] Git version and Env stuff

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,25 @@ Bundler.require(*Rails.groups)
 
 module SecretAdvisor
   class Application < Rails::Application
-    config.time_zone = 'Central Time (US & Canada)'
-    config.i18n.default_locale = :en
+    VERSION = "0.0.0"
+    
+    config.time_zone = ENV["SA_TIME_ZONE"] || "Brasilia"
+    config.i18n.default_locale = ENV["SA_DEFAULT_LOCALE"] || :en
     config.active_record.raise_in_transactional_callbacks = true
+  
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: true,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
