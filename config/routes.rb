05e293b554459051e0a9aa22ebf3ce41b95a5007
Marcelo Boeira <me@marceloboeira.com>
2015-05-22 22:04:58 -0300

    Devise views, routes and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :users,
+  path: "account",
+  path_names: {
+    sign_up: "sign-up",
+    sign_in: "sign-in",
+    sign_out: "sign-out",
+    password: "password",
+    confirmation: "verify",
+    edit: "settings"
+  }
+
 end
