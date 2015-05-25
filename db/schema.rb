056e24c25ce3acd1ec4d5a4434ea88809bc8ca82
Marcelo Boeira <me@marceloboeira.com>
2015-05-25 14:23:53 -0300

    [#10] Schema and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512005213) do
+ActiveRecord::Schema.define(version: 20150530152429) do
+
+  create_table "advices", force: :cascade do |t|
+    t.integer  "advisor_id"
+    t.integer  "advised_id"
+    t.text     "message"
+    t.boolean  "anonymously", default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
