dc08a739a9976c51059984399e569a6202810e29
Marcelo Boeira <me@marceloboeira.com>
2015-11-15 17:45:49 -0200

    test: Fix tests with shoulda matchers validation

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails', '~> 3.0'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.new }
-  it "check presence of a value to the symbol name" do 
-    expect(user.name).to_not be_nil 
-  end
 
+  it "check presence of a value to the symbol name" do
+    expect(user).to validate_presence_of(:name)
+  end
 end
